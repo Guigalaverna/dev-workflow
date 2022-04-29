b55e9d3b24061686fdf9f547c827e4a9c4f1a870
Guigalaverna <guigalaverna@gmail.com>
2022-04-29 16:24:11 -0300

    feat: runnning all scripts in folder

--- install.sh
+++ install.sh
@@ -2,21 +2,12 @@
 
 sudo chmod +x scripts/*.sh
 
-# colors
-BLUE='\033[1;36m'
-NC='\033[0m'
+# load configs
 
-clear
-echo -e "Welcome to ${BLUE}Arch Linux${NC} Installer!"
-echo ""
-echo -e "${BLUE}[1]${NC} Install from LiveCD of Arch Linux"
-echo -e "${BLUE}[2]${NC} Install apps and setup computer"
-echo ""
+SCRIPT_LIST=($(ls -l ./scripts | awk '{print $9}'))
 
-read -p "Choose one option: " option
+clear # clear term
 
-if [ option = 1 ]; then
-    ./scripts/full-install.sh
-else
-    ./scripts/app-install.sh
-fi
+for script in ${SCRIPT_LIST[@]}; do
+    sudo bash ./scripts/$script
+done
