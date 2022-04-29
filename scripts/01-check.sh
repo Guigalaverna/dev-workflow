b23133178b754009d0e8733fd1161b7af4868aa7
Guigalaverna <guigalaverna@gmail.com>
2022-04-29 16:25:57 -0300

    refactor: remove unnecessary checks

--- scripts/01-check.sh
+++ scripts/01-check.sh
@@ -1,28 +1,16 @@
 # load configs
 source './config/colors.conf'
-source './config/system.conf'
 source './config/functions.sh'
-
-clear # clear term
 
 print_step 01 Starting check
 
 echo ""
 
-# check boot mode
-DIR_EFI_VARS="/sys/firmware/efi/efivars"
-if [ -d $DIR_EFI_VARS ]; then
-    print_message BIOS "Boot mode (UEFI)"
-else
-    print_error "This script dosen't have support to BIOS mode"
-    exit
-fi
-
 # check wifi
 ping -c1 google.com 1> /dev/null 2> /dev/null # use ping to check
 
 if [ $? -eq 0 ]; then 
-    print_message Network "Have connection"
+    print_check Network "Have connection"
 else
     echo ""
     print_error "No internet connection available"
@@ -31,11 +19,3 @@ fi
 
 # update system clock
 timedatectl set-ntp true
-
-# check if service status
-timedatectl status | grep $TIMEZONE > /dev/null
-if [ $? -eq 0 ]; then
-    print_message Timezone "The system timezone is correct"
-else
-    print_error "The timezone set is not correct with in ./config/system.conf"
-fi
